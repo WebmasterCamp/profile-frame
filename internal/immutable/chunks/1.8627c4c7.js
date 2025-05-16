5336f9dd76fc8e6bc1af2af83eb48cd509d1220a
Vinniel <155176879+Vinniel5403@users.noreply.github.com>
2025-05-16 23:00:19 +0700

    update frame

--- internal/immutable/chunks/1.8627c4c7.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.2ae6e795.js";export{t as component};
--- internal/immutable/chunks/2.a73ef418.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.348ed332.js";export{t as component};
